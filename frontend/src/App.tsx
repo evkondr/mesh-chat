cd0e595d854d54529a6cf8a12578c7a9f6a49e05
evkondr <kondratiev.evg@gmail.com>
2026-03-09 12:24:18 +0300

    init: front

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router"
+import ChatPage from "./pages/ChatPage"
+import LoginPage from "./pages/LoginPage"
+import SignUpPage from "./pages/SignUpPage"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 relative items-center justify-center p 4 overflow-hidden">
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
