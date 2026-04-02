59cbe0a052a5e5ed39ab91fa288ca4d60bacc4fa
evkondr <kondratiev.evg@gmail.com>
2026-04-02 22:45:49 +0300

    feat: configurate socket connection

--- backend/src/utils/socket.ts
+++ backend/src/utils/socket.ts
@@ -1,6 +1,8 @@
+import socketAuthMiddleware from '@/middleware/socketAuthMiddleware';
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+
 const app = express();
 const server = http.createServer(app);
 
@@ -10,3 +12,25 @@ const io = new Server(server, {
     credentials: true
   }
 });
+
+io.use(socketAuthMiddleware);
+
+const onlineUsers:Record<string, string> = {};
+
+io.on('connection', (socket) => {
+  console.log('User connected: ', socket.user.fullName);
+  const userId = socket.user.id;
+  onlineUsers[userId] = socket.id;
+  io.emit('getOnlineUsers', Object.keys(onlineUsers));
+  socket.on('disconnect', () => {
+     console.log('User disconnected: ', socket.user.fullName);
+     delete onlineUsers[userId];
+     io.emit('getOnlineUsers', Object.keys(onlineUsers));
+  });
+});
+
+export {
+  io,
+  app,
+  server
+};
